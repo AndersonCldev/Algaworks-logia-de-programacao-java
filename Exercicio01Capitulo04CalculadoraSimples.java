d7f08abc7a967f4f0dd14a883152e681bdca25f5
Anderson Cleiton <135235923+AndersonCldev@users.noreply.github.com>
2023-10-07 09:34:00 -0300

    Calculadora simples
    # Este é um programa Java que implementa uma calculadora simples de quatro operações: adição, subtração, multiplicação e divisão. O usuário insere dois números e escolhe a operação desejada através de um menu.
    
    # Funcionalidades
    O programa permite ao usuário escolher entre quatro operações: multiplicação, divisão, adição e subtração.
    O usuário insere dois números.
    O resultado da operação escolhida é exibido no console.

---
+++ Exercicio01Capitulo04CalculadoraSimples.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Exercicio01Capitulo04CalculadoraSimples {
+	public static void main(String[] args) {
+		Integer PrimeiroNumero = 0;
+		Integer segundoNumero = 0;
+		Integer operacao = 0; 
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.print("Olá, digite por favor o primeiro número para primeira operação:");
+		PrimeiroNumero = ler.nextInt();
+		
+		System.out.println("DIGITE O NÚMERO CORRESPONDENTE AO SIMBOLO DA OPERAÇÃO QUE SERÁ REALIZADA. (EX: : 1 Multiplicação , 2 Divisão, 3 Adição, 4 Subtração.) :  ");
+		operacao = ler.nextInt();
+		
+		System.out.print("Pronto, agora digite o segundo número da operação: ");
+		segundoNumero = ler.nextInt();
+		
+			if (operacao == 1 ) {
+				
+				System.out.println("Resultado: " + PrimeiroNumero * segundoNumero);	
+				
+			}else if (operacao == 2) {
+				
+				System.out.println("Resultado: "+ PrimeiroNumero / segundoNumero);
+				
+			}else if(operacao == 3){
+				
+				System.out.println("Resultado: "+ (PrimeiroNumero + segundoNumero));
+				
+			}else if(operacao == 4 ) {
+				
+				System.out.println("Resultado: " + (PrimeiroNumero - segundoNumero));
+			}else {
+				System.out.println("Seu vagabundo não existe operação na opção número " + operacao + ""+" Reinicie o sistema.");
+			}
+			
+				ler.close();
+			
+			
+				
+	
+		}
+	
+	}
+
