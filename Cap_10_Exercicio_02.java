1fdb5ed02f16f9622b8095f2ed8c1983a53edcd3
Anderson Cleiton <135235923+AndersonCldev@users.noreply.github.com>
2024-03-11 12:52:54 -0300

    Update Cap_10_Exercicio_02.java
    Neste exercício incluir adicionamento de 4 nomes e fiz com que o programa guardasse esses nomes na pasta informada.

--- Cap_10_Exercicio_02.java
+++ Cap_10_Exercicio_02.java
@@ -3,15 +3,29 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class gravandoArquivos {
 
 	public static void main(String[] args) throws IOException {
+		
 		ArrayList<String> linhas = new ArrayList<String>();
-		linhas.add("Anderson");
+		Scanner ler = new Scanner(System.in);
+		System.out.println("Escreva 4 nomes:" );
+		
+		for(int i = 0; i < 4 ; i ++) {
+			
+			String nome = ler.nextLine();
+			linhas.add(nome);
+		}
+		
+		System.out.println("Fim");
+
+		
+		
 		Path arquivo = Paths.get("C:\\Users\\ander\\OneDrive\\Área de Trabalho\\AlgaworksAUlas\\teste.txt");
 		Files.write(arquivo, linhas);
-
 	}
+	
 
 }
