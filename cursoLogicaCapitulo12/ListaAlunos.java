fc614e4fd06ee06210f746e28c777360ac361d71
Anderson Cleiton <135235923+AndersonCldev@users.noreply.github.com>
2024-03-16 11:33:44 -0300

    Update ListaAlunos.java

--- cursoLogicaCapitulo12/ListaAlunos.java
+++ cursoLogicaCapitulo12/ListaAlunos.java
@@ -2,7 +2,7 @@ package cursoLogicaCapitulo12;
 
 	public class ListaAlunos {
 		
-		static final int QUANTIDADE_LISTA = 2;
+		static final int QUANTIDADE_LISTA = 5;
 		
 		Aluno [] lista = new Aluno[QUANTIDADE_LISTA];
 		
@@ -27,8 +27,40 @@ package cursoLogicaCapitulo12;
 				}
 				
 				lista = novaLista;
+				
+				
+				
 			}
 			lista [tamanhoLista] = aluno;
 			tamanhoLista ++;
 		}
+		
+		
+		void remover(Aluno aluno) {
+			for(int i =0; i< tamanhoLista; i++ ) {
+				Aluno a = lista[i];
+						
+						if(a!= null && a.equals(aluno)) {
+							remover(i);
+							break;
+							
+						}else if ( a == null && aluno == null) {
+							remover(i);
+							break;
+						}
+			}
+		}
+		
+		void remover(int indice1) {
+			int indiceIncial = indice1 + 1;
+			
+			for(int i = indiceIncial ; i < tamanhoLista; i++) {
+				lista[i - 1] = lista[indiceIncial]; 	
+				}
+			tamanhoLista--;
+			lista[tamanhoLista] = null;
+		}
+		
+		
+		
 }
