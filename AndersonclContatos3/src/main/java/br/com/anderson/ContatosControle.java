e893f17b60cdca80259de4e0e5163e3378449bc2
Anderson Cleiton <135235923+AndersonCldev@users.noreply.github.com>
2024-04-10 11:26:53 -0300

    Update ContatosControle.java
    Criação de função para alteração dos contatos ja existentes, assim sendo possível alterar nome e número. Também implementamos a opção de remover contatos cadastrados.

--- AndersonclContatos3/src/main/java/br/com/anderson/ContatosControle.java
+++ AndersonclContatos3/src/main/java/br/com/anderson/ContatosControle.java
@@ -1,81 +1,114 @@
-package br.com.anderson;
-import java.util.ArrayList;
+	package br.com.anderson;
+	import java.util.ArrayList;
 import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-
 public class ContatosControle {
-		
+	
 	private static final ArrayList<Contato> LISTA_CONTATOS = new ArrayList<>();
 	
 	static {
-			//lista de contatos pre adicionados 
-		LISTA_CONTATOS.add(new Contato("1", "Maria", "+55 71 98565 6755"));
-		LISTA_CONTATOS.add(new Contato("2", "José", "+55 71 9789 6541"));
-		LISTA_CONTATOS.add(new Contato("3", "Cleitom", "+55 71 99756 3942"));
-		LISTA_CONTATOS.add(new Contato("4", "Terma", "+55 71 95555 5555"));
-		
+		LISTA_CONTATOS.add(new Contato("1", "Maria", "+55 71 00000 0000"));
+		LISTA_CONTATOS.add(new Contato("2", "Anderson", "+55 71 00000 0000"));
+		LISTA_CONTATOS.add(new Contato("3", "Cleiton", "+55 71 00000 0000"));
+		LISTA_CONTATOS.add(new Contato("4", "Jesus", "+55 71 00000 0000"));
+		LISTA_CONTATOS.add(new Contato("5", "Thaylane", "+55 71 00000 0000"));
 	}
 	
-	//Chamado do index 
 	@GetMapping("/")
 	public String index() {
-			return "index" ;
-		}
+		return "index";
+	}
 	
-	//chamado da aba contatos
 	@GetMapping("/contatos")
 	public ModelAndView listar() {
 		ModelAndView modelAndView = new ModelAndView("listar");
+		
 		modelAndView.addObject("contatos", LISTA_CONTATOS);
+		
 		return modelAndView;
 	}
 	
-	//chamados da aba adicona novo contato
 	@GetMapping("/contatos/novo")
 	public ModelAndView novo() {
 		ModelAndView modelAndView = new ModelAndView("formulario");
+		
 		modelAndView.addObject("contato", new Contato());
 		
 		return modelAndView;
-	}
+	}	
 	
-	
-	@PostMapping ("/contatos")
+	@PostMapping("/contatos")
 	public String cadastrar(Contato contato) {
 		String id = UUID.randomUUID().toString();
+		
 		contato.setId(id);
+		
 		LISTA_CONTATOS.add(contato);
+		
 		return "redirect:/contatos";
 	}
 	
-	// implementação da edição dos contatos ja cadastrados 
 	@GetMapping("/contatos/{id}/editar")
-	public ModelAndView editar(@PathVariable String id ) {
+	public ModelAndView editar(@PathVariable String id) {
 		ModelAndView modelAndView = new ModelAndView("formulario");
-		modelAndView.addObject("contato", new Contato());
 		
-		Contato contato = prcurarContato(id);
-		modelAndView.addObject("contato" , contato);
+		Contato contato = procurarContato(id);
+		
+		modelAndView.addObject("contato", contato);
+		
 		return modelAndView;
 	}
 	
-	//aqui validamos se o contato ja existe 
-	public Contato prcurarContato(String id) {
+	@PutMapping("/contatos/{id}")
+	public String atualizar(Contato contato) {
+		Integer indice = procurarIndiceContato(contato.getId());
+		
+		Contato contatoVelho = LISTA_CONTATOS.get(indice);
+		
+		LISTA_CONTATOS.remove(contatoVelho);
+		
+		LISTA_CONTATOS.add(indice, contato);
+		
+		return "redirect:/contatos";
+	}
+	
+	@DeleteMapping("/contatos/{id}")
+	public String remover(@PathVariable String id, Contato contato) {
+		LISTA_CONTATOS.remove(contato);
+		return "redirect:/contatos";
+	}
+	
+	// -------------------------------------- Métodos auxiliares
+	
+	private Contato procurarContato(String id) {
+		Integer indice = procurarIndiceContato(id);
+		
+		if (indice != null) {
+			Contato contato = LISTA_CONTATOS.get(indice);
+			return contato;
+		}
+		
+		return null;
+	}
+	
+	private Integer procurarIndiceContato(String id) {
 		for (int i = 0; i < LISTA_CONTATOS.size(); i++) {
 			Contato contato = LISTA_CONTATOS.get(i);
 			
-			if(contato.getId().equals(id)) {
-				return contato;
+			if (contato.getId().equals(id)) {
+				return i;
 			}
 		}
+		
 		return null;
 	}
 }
-
